e1b3c268db0f7c99fbf0c57c521fc76222abd6cf
Rajesh Kumar <rajesh157@gmail.com>
2022-08-29 23:46:12 +0530

    c5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 0.625
+          Learn React 0.64
         </a>
       </header>
     </div>
