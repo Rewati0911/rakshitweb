19f4b4ebed97bf3164ca25f17fc1a8abdb9ac53e
rewati0911 <rewati0911@gmail.com>
2022-08-29 12:08:29 +0530

    rewati 3 comment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 0.3
+          Learn React 0.4
         </a>
       </header>
     </div>
