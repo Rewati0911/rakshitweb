cdf56b5fd002a27536af727257ebacdd2c955722
rewati0911 <rewati0911@gmail.com>
2022-08-29 13:17:05 +0530

    udpate 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 0.4
+          Learn React 0.5
         </a>
       </header>
     </div>
