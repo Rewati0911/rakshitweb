b52df8b943cc194e43705e37c4618ba27eff68a4
Rajesh Kumar <rajesh157@gmail.com>
2023-03-24 18:08:41 +0530

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>hi
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        1
+        2
         <a
           className="App-link"
           href="https://reactjs.org"
