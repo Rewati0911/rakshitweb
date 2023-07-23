1fc4631f19a06ead75769940ac46435411e92ccf
Rajesh Kumar <rajesh157@gmail.com>
2023-07-23 22:36:42 +0530

    udpate

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>hi 1
-          Edit <code>src/App.js</code> and save to reload1.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         2
         <a
